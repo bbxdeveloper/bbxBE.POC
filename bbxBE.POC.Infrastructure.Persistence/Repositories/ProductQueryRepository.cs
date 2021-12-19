b8c296b54d80aed7fcaa27e94afe6310aed56af0
Jeremi <kdrisaac42@gmail.com>
2021-12-19 12:28:13 +0100

    ConcurrentBag, only one cache for searches. Updated appsettings.json parameters.

--- bbxBE.POC.Infrastructure.Persistence/Repositories/ProductQueryRepository.cs
+++ bbxBE.POC.Infrastructure.Persistence/Repositories/ProductQueryRepository.cs
@@ -6,6 +6,7 @@ using bbxBE.POC.Infrastructure.Persistence.Query;
 using Dapper;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,6 +25,7 @@ namespace bbxBE.POC.Infrastructure.Persistence.Repositories
         private const string QUERY_PRODUCTS_FOR_SEARCH = "SELECT TOP (@topCount) TERMKOD, TERMNEV FROM CTRZS";
 
         private const string SEARCH_PRODUCTS = "SELECT TOP (@topCount) TERMKOD, TERMNEV FROM CTRZS WHERE TERMKOD LIKE @searchString OR TERMNEV LIKE @searchString";
+        private const string COUNT_SEARCH_PRODUCTS = "SELECT COUNT(*) FROM CTRZS WHERE TERMKOD LIKE @searchString OR TERMNEV LIKE @searchString";
         private const string SEARCH_PRODUCTS_BY_CODE = "SELECT TOP (@topCount) TERMKOD, TERMNEV FROM CTRZS WHERE TERMKOD LIKE @searchString";
         private const string SEARCH_PRODUCTS_BY_NAME = "SELECT TOP (@topCount) TERMKOD, TERMNEV FROM CTRZS WHERE TERMNEV LIKE @searchString";
 
@@ -35,9 +37,16 @@ namespace bbxBE.POC.Infrastructure.Persistence.Repositories
         private const string EMPTY_SEARCH_RESULT = "Nem található a keresésnek megfelelő termék!";
         private const string EMPTY_RESULT = "Nem található megjeleníthető termék!";
 
+        private const string SEARCH_TOO_MUCH_RESULT = "A találatok száma ({0}) meghaladja a maximális megengedett mennyiséget ({1}). Kérem próbálja meg pontosítani a keresését!";
+        private const string QUERY_TOO_MUCH_RESULT = "A találatok száma ({0}) meghaladja a maximális megengedett mennyiséget ({1}). Kérem próbálja meg szűrni a találatokat egy kereséssel!";
+
+        private const string RESULT_COUNT = "Találatok száma: {0}, megjelenített: {1}";
+
+        private const string ERROR = "Hiba: {0}";
+
         #endregion Messages
 
-        private readonly Dictionary<string, IEnumerable<CTRZS>> _SearchCache;
+        private readonly ConcurrentBag<CTRZS> _searchCache;
 
         private readonly DapperContext _context;
 
@@ -52,7 +61,7 @@ namespace bbxBE.POC.Infrastructure.Persistence.Repositories
 
             _rowCountThreshold = _configuration.GetValue<int>("RowCountThreshold");
 
-            _SearchCache = new Dictionary<string, IEnumerable<CTRZS>>();
+            _searchCache = new ConcurrentBag<CTRZS>();
 
             _cacheExpirationInMinutes = _configuration.GetValue<int>("CacheExpirationInMinutes");
             _cacheLastHeartBeat = DateTime.UtcNow;
@@ -64,38 +73,58 @@ namespace bbxBE.POC.Infrastructure.Persistence.Repositories
             if ((momentOfCheck - _cacheLastHeartBeat).TotalMinutes >= _cacheExpirationInMinutes)
             {
                 _cacheLastHeartBeat = momentOfCheck;
-                _SearchCache.Clear();
+                _searchCache.Clear();
             }
         }
 
         public async Task<ProductListQueryResponse> QueryForSearch(ProductListQueryRequest req)
         {
-            using (var connection = _context.CreateConnection())
+            var res = new ProductListQueryResponse
             {
-                var res = new ProductListQueryResponse
-                {
-                    Result = new List<CTRZS>(),
-                    IsError = false
-                };
+                Result = new List<CTRZS>(),
+                IsError = false
+            };
 
-                Math.Clamp(req.TopCount, 1, _rowCountThreshold);
+            try
+            {
+                using var connection = _context.CreateConnection();
+
+                req.TopCount = Math.Clamp(req.TopCount, 1, _rowCountThreshold);
 
                 ClearCacheIfExpired();
 
-                _SearchCache.Remove(nameof(SearchProduct));
-
-                if (_SearchCache.ContainsKey(nameof(QueryForSearch)))
+                var count = (await connection.QueryAsync<int>(COUNT_PRODUCTS,
+                    new DynamicParameters(new Dictionary<string, object>() { { "@searchString", req.SearchString } }))).FirstOrDefault();
+                if (count > _rowCountThreshold)
                 {
-                    res.Result = _SearchCache[nameof(QueryForSearch)];
+                    res.Message = string.Format(QUERY_TOO_MUCH_RESULT, count, _rowCountThreshold);
+                }
+                else
+                {
+                    res.Message = string.Format(RESULT_COUNT, count, req.TopCount);
+                }
+
+                if (!_searchCache.IsEmpty)
+                {
+                    res.Result = _searchCache.ToList();
 
                     if (res.Result.Any() && res.Result.Count() >= req.TopCount)
                     {
+                        if (res.Result.Count() > _rowCountThreshold)
+                        {
+                            res.Message = string.Format(QUERY_TOO_MUCH_RESULT, res.Result.Count(), _rowCountThreshold);
+                        }
+                        else
+                        {
+                            res.Message = string.Format(RESULT_COUNT, count, req.TopCount);
+                        }
+
                         res.Result = res.Result.Take(req.TopCount);
                         return res;
                     }
                     else
                     {
-                        _SearchCache.Remove(nameof(QueryForSearch));
+                        _searchCache.Clear();
                     }
                 }
 
@@ -113,59 +142,83 @@ namespace bbxBE.POC.Infrastructure.Persistence.Repositories
 
                 res.Result = products;
 
-                _SearchCache.Add(nameof(QueryForSearch), products);
+                foreach (var p in products)
+                {
+                    _searchCache.Add(p);
+                }
 
+                return res;
+            }
+            catch (Exception ex)
+            {
+                res.IsError = true;
+                res.Message = string.Format(ERROR, ex.Message); ;
                 return res;
             }
         }
 
         public async Task<ProductListQueryResponse> SearchProduct(ProductListQueryRequest req)
         {
-            using (var connection = _context.CreateConnection())
+            var res = new ProductListQueryResponse
             {
-                var res = new ProductListQueryResponse
-                {
-                    Result = new List<CTRZS>(),
-                    IsError = false
-                };
+                Result = new List<CTRZS>(),
+                IsError = false
+            };
 
-                Math.Clamp(req.TopCount, 1, _rowCountThreshold);
+            try
+            {
+                using var connection = _context.CreateConnection();
+
+                req.TopCount = Math.Clamp(req.TopCount, 1, _rowCountThreshold);
 
                 if (!string.IsNullOrWhiteSpace(req.SearchString))
                 {
 
                     ClearCacheIfExpired();
 
-                    if (_SearchCache.ContainsKey(nameof(SearchProduct)))
+                    var count = (await connection.QueryAsync<int>(COUNT_SEARCH_PRODUCTS,
+                        new DynamicParameters(new Dictionary<string, object>() { { "@searchString", req.SearchString } }))).FirstOrDefault();
+                    if (count > _rowCountThreshold)
                     {
-                        res.Result = _SearchCache[nameof(SearchProduct)];
+                        res.Message = string.Format(SEARCH_TOO_MUCH_RESULT, count, _rowCountThreshold);
+                    }
+                    else
+                    {
+                        res.Message = string.Format(RESULT_COUNT, count, req.TopCount);
+                    }
+
+                    // At first, we try to do the quicksearch in the cache
+                    var cacheSearch = _searchCache.Where(c => c.TERMKOD.Contains(req.SearchString) || c.TERMNEV.Contains(req.SearchString));
+                    if (cacheSearch.Any())
+                    {
+                        res.Result = cacheSearch;
 
                         if (res.Result.Any() && res.Result.Count() >= req.TopCount)
                         {
-                            // At first, we try to do the quicksearch in the cache
-                            res.Result = res.Result.Where(x => x.TERMKOD.Contains(req.SearchString)).Take(req.TopCount);
-
-                            // In case of no match, we run the query on the database - and clear the cache from the previous try.
-                            if (!res.Result.Any())
+                            if (res.Result.Count() > _rowCountThreshold)
                             {
-                                _SearchCache.Remove(nameof(SearchProduct));
+                                res.Message = string.Format(SEARCH_TOO_MUCH_RESULT, res.Result.Count(), _rowCountThreshold);
                             }
                             else
                             {
-                                return res;
+                                res.Message = string.Format(RESULT_COUNT, count, req.TopCount);
                             }
+
+                            res.Result = res.Result.Take(req.TopCount);
+                            return res;
                         }
+                        // In case of no match, we run the query on the database - and clear the cache from the previous try.
                         else
                         {
-                            _SearchCache.Remove(nameof(SearchProduct));
+                            _searchCache.Clear();
                         }
                     }
 
                     var parameters = new DynamicParameters(new Dictionary<string, object>
-                    {
-                        { "@topCount", req.TopCount },
-                        { "@searchString", req.SearchString.EncodeForLikeOperator() },
-                    });
+                        {
+                            { "@topCount", req.TopCount },
+                            { "@searchString", req.SearchString.EncodeForLikeOperator() },
+                        });
                     var products = await connection.QueryAsync<CTRZS>(SEARCH_PRODUCTS, parameters);
 
                     if (!products.Any())
@@ -176,7 +229,10 @@ namespace bbxBE.POC.Infrastructure.Persistence.Repositories
 
                     res.Result = products;
 
-                    _SearchCache.Add(nameof(SearchProduct), products);
+                    foreach (var p in products)
+                    {
+                        _searchCache.Add(p);
+                    }
 
                     return res;
                 }
@@ -184,6 +240,12 @@ namespace bbxBE.POC.Infrastructure.Persistence.Repositories
                 res.IsError = true;
                 res.Message = EMPTY_SEARCHSTRING;
 
+                return res;
+            }
+            catch (Exception ex)
+            {
+                res.IsError = true;
+                res.Message = string.Format(ERROR, ex.Message); ;
                 return res;
             }
         }
